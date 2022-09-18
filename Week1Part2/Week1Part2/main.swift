687397a4381521e487fb6b6706173f2f568206e0
MustafaEraslan <eraslan.mustafa@outlook.com>
2022-09-18 12:54:40 +0300

    closure, Delegate, notification center

--- Week1Part2/Week1Part2/main.swift
+++ Week1Part2/Week1Part2/main.swift
@@ -19,3 +19,53 @@ struct User {
         self.age = age
     }
 }
+
+//mark: - Generics
+protocol Provider {
+    var baseUrl: String { get }
+    var endPoint: String { get }
+    var parameters: [ String : String] { get }
+}
+
+enum InstagramProvider {
+    case getRecentPosts
+    case getProfile
+    case search(text: String)
+}
+
+//extension InstagramProvider: Provider {
+//    var baseUrl: String{
+//        "https://instagram.com"
+//    }
+//    var endPoint: String{
+//        switch self {
+//        case .getProfile:
+//            return "/getProfile"
+//        case .getRecentPosts:
+//            return "/getRecentPhotos"
+//        case .search:
+//        }
+//    }
+//}
+
+// Mark: closure, Delegate, notification center
+class Button {
+    var isTapped: Bool = false
+    func tap() {
+        isTapped = true
+    }
+    
+    func untap() {
+        isTapped = false
+    }
+}
+
+var button = Button()
+
+button.tap()
+
+if button.isTapped {
+    print ("Button Tapped")
+}
+
+
